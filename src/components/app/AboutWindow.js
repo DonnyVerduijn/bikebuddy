0e0e4e504894ee49668df22773eee3bb67c86d46
DonnyVerduijn <donnyverduijn@gmail.com>
2019-01-11 15:16:48 +0100

    update about window

--- src/components/app/AboutWindow.js
+++ src/components/app/AboutWindow.js
@@ -2,12 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Window from '../common/Window';
 import AboutNavigationContainer from '../containers/AboutAppBar';
+import Typography from '@material-ui/core/Typography'
 
 const AboutWindow = () => {
   return (
     <Window>
       <AboutNavigationContainer />
-      <h1>About</h1>
+      <Typography variant="h1">About</Typography>
+      <Typography>
+        This application is developed by Donny Verduijn as a school assignment.
+        It provides a way to find back your bike in large bicycle storages.
+      </Typography>
     </Window>
   );
 };
